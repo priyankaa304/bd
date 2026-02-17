c5e38f9323675cf390aa2390d5de8db79d9b3638
priyankaa304 <npriyankabsccse25@rvu.edu.in>
2026-02-18 00:08:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToGallery() {
+  document.getElementById("page1").classList.add("hidden");
+  document.getElementById("page2").classList.remove("hidden");
+}
+
+function goToEnd() {
+  document.getElementById("page2").classList.add("hidden");
+  document.getElementById("page3").classList.remove("hidden");
+}
